4796eba07678f1497dd7896cb487408d50b11b0b
BookerLights <axel_booker@hotmail.com>
2024-06-10 21:12:09 -0300

    mi primer commit

---
+++ ingresar-numero.py
@@ -0,0 +1,7 @@
+# Pedir al usuario que ingrese un número
+numero = input("Por favor, ingresa un número: ")
+
+#Imprimir el número ingresado
+print("\n\n" + "-"*30)
+print(f"El número que ingresaste es: {numero}")
+print("-"*30)
---
+++ listas.py
@@ -0,0 +1,8 @@
+#LISTAS
+
+nombres = ["Axel","Juan","Mica", "Mariano","Marcos", "Agustin",]
+
+for nombre in nombres:
+    print(nombre)
+
+
